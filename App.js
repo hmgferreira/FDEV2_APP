2bfc9272fd56c0a8511f9f5a403e9dc25a1074fa
Márcio Ferreira <hmarciogferreira@gmail.com>
2024-02-24 14:26:12 -0300

    Token

--- App.js
+++ App.js
@@ -3,11 +3,23 @@ import { StyleSheet, Text, View } from 'react-native';
 import PublicRoutes from './src/routes/PublicRoutes';
 import PrivateRoutes from './src/routes/PrivateRoutes';
 import AuthContext from './src/contexts/AuthContext';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-gesture-handler';
+import Util from './src/config/Util';
 
 export default function App() {
   const[isLogged, setIsLogged] = useState(false);
+
+  async function checkToken() {
+    const token = await Util.getToken();
+    if(token!== null) {
+      setIsLogged(true);
+    }
+  }
+
+  useEffect(() => {
+    checkToken();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ isLogged, setIsLogged }}>
